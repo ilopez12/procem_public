c2f8111a3c80a1e0525a88c83d09b90498185341
“Isis <i.loopez12@gmail.com>
2025-07-06 12:00:52 -0500

    reconstruction page

---
+++ indexnormal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+    <TItle>Procem Internacional</TItle>
+
+<!-- FINAL DEL HEAD INICIO DEL BODY HEADER  -->
+    <?php include 'views/Partials/header.php'; ?>
+<!-- TAMBIEN INCLUYE EL FINAL DEL  HEADER  -->
+
+<!-- FIN DEL INVENTO -->
+
+<!-- FIN QUINTA PARTE -->
+<?php include 'views/Partials/footer.php'; ?>
+</body>
+</HTML>
