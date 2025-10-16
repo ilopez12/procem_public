4f2b930332351dc9fe038482a78b2684157eebf4
“Isis <i.loopez12@gmail.com>
2025-10-15 23:58:28 -0500

    update index headers

---
+++ _index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+    <TItle>Procem Internacional</TItle>
+
+<!-- FINAL DEL HEAD INICIO DEL BODY HEADER  -->
+    <?php include 'views/Partials/header.php'; ?>
+<!-- TAMBIEN INCLUYE EL FINAL DEL  HEADER  -->
+<div><!-- INICIO DEL INVENTO -->
+
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="img/culturaempresarial/baner2.png" class="plearning_banner d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/culturaempresarial/baner3.png" class="plearning_banner d-block w-100" alt="...">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+    </a>
+    </div>
+
+    <!-- PRIMERA PARTE -->
+    <DIV id="principal" class="par2 hvr-underline-from-center">
+        <div class="container">
+            <div class="general">
+                <h3>SOMOS PROCEM</h3>
+              <div data-aos="fade-up" data-aos-duration="2000">
+                <h5 class=" parrafo1">La Fundación Procem Internacional de Panamá es una organización sin fines de lucro, que como sus siglas lo indican somos una fundación Pro Cultura Empresarial</h5>
+              </div>
+              </div>
+        <div class="general">
+        <div class="row">
+                <div class="col-sm-6">
+                    <div class="imagen_principal">
+                    <img class="principal" src="img/culturaempresarial/procem.png" alt="">
+                    </div>
+                </div>
+          
+                <div class="col-lg-6" >
+                  <div data-aos="fade-up" data-aos-duration="3000">
+                    <h3>LEY 128 DEL 3 DE MARZO DE 2020</h3>
+                    <p class="parrafo1">Luego de 15 años de gestionar el proyecto ley, se hace realidad, como una herramienta educativa cónsona con la realidad global.
+                        Esta ley establece la enseñanza obligatoria de la Cultura Empresarial y del Emprendimiento como eje transversal en asignaturas.
+                    </p>
+                    <div style="text-align: center">
+                        <a id="imagen" href="views/principal/leer_mas.php?nombre=lee_procem" class="boton btn btn-primary">Leer más...</a>
+                    </div>
+                </div>
+          </div>
+                </div>
+            </div>
+
+        </div>
+    </DIV>
+    <!-- FIN PRIMERA PARTE -->
+    <!-- SEGUNDA PARTE -->
+    <DIV id="second" class="none2 hvr-underline-from-center2">
+            <div class="centro panelsombreado">
+            <div class="container">
+
+                <h3>PROCEM EN PRO DE LA EDUCACIÓN</h3>
+                <div class="general">
+                  <div data-aos="fade-up" data-aos-duration="3000">
+                <p class=" parrafo2">La Fundación Procem funciona para la educación internacional buscando enseñar, capacitar, asesorar y dirigir la Cultura Empresarial a través de la metodología Triangular de las Artes, inspirada por la Dra. AnaMae de Barbosa de Brasil</p>
+                  </div>
+                </div>
+                <div class="filas "></div>
+            </div>
+            </div>
+    </DIV>
+    <!-- FIN SEGUNDA PARTE -->
+    <!-- CUARTA  PARTE -->
+    <DIV id="third" class="par">
+        <div class="container">
+        <div class="general">
+            <div class="row">
+               
+                <div class="col-md-12 mt-md-0 mt-4">
+                	<h3 class="centro"><img class="principal" src="img/procem/pcampus.png" style="width: 40%;height: 15%;" alt=""></h3>
+                    <div class="general">
+                      <div data-aos="fade-up" data-aos-duration="3000">
+                          <p class=" parrafo3">La plataforma educacional "P-CAMPUS" será la mejor escuela internacional para nuestros niños, jóvenes y adultos con una educación integral,<br> con el fin de tener a Panamá por encima de todos los países en la educación virtual y semi presencial.
+                          </p>
+                      
+
+                      </div>
+                    </div>
+                  
+                </div>
+                
+            </div>
+            </div> 
+        </div>
+    </DIV>
+    <!-- FIN CUARTA PARTE -->
+    <!-- TERCERA PARTE -->
+    <DIV  id= "fourth" class="none hvr-underline-from-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mt-md-0 mt-6">
+                    <div class="centro">
+                        <div class="general">
+                        <h3 style="color: white;">¿QUÉ ES LA CULTURA EMPRESARIAL?</h3>
+                          <div data-aos="fade-up" data-aos-duration="1000">
+                        <p class=" parrafo4">La cultura Empresarial es el ADN de todo ser humano individual en su mundo cotidiano, comercial, técnico y científico mediante la creatividad, organizacional y productividad.</p>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-md-0 mt-6">
+                    <div class="centro">
+                        <div class="general">
+                            <div class="columnas">
+                            <h3 style="color: white;">¿CÓMO IMPACTA EN LA JUVENTUD?</h3>
+                              <div data-aos="fade-up" data-aos-duration="3000">
+                            <p class=" parrafo4">La Cultura Empresarial impacta a la juventud en toda su organización como ser pensante, analítico y crítico.
+                                Por medio del estudio profundo de su ser o como hoy día se refiere a su marca. La juventud está preparada para
+                                enfrentarse a situaciones adversas con una Cultura Empresarial.
+                            </p>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </DIV>
+    <!-- FIN TERCERA PARTE -->
+
+
+    <!-- QUINTA PARTE -->
+    <DIV id="fifth" class="par ">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-12">
+                    <div class="centro">
+                        <div class="general">
+                        <h3>¿CÓMO PUEDO SER PARTE DE PROCEM?</h3>
+                          <div data-aos="fade-up" data-aos-duration="3000">
+                        <p class=" parrafo5">Todos pueden ser parte de PROCEM, como estudiantes, maestros y profesionales en diferentes ramas como padrinos, inversionistas y voluntarios. La Fundación Procem Internacional necesita del interés internacional de productividad en contra de la pobreza.</p>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12" style="text-align:center;">
+                    <img class="principal2" src="img/f-icons/5.png" alt="">
+                </div>
+
+            </div>
+        </div>
+    </DIV>
+</div>
+<!-- FIN DEL INVENTO -->
+
+<!-- FIN QUINTA PARTE -->
+<?php include 'views/Partials/footer.php'; ?>
+</body>
+</HTML>
--- index.php
+++ index.php
@@ -5,172 +5,37 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="descriptison">
     <meta content="" name="keywords">
+        
+    <link rel="shortcut icon" href="img/procem/logo.png">
+<link href="img/favicon.png" rel="icon">
+<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+
+    <link href="css/general.css" rel="stylesheet">
+    <link href="css/animaciones.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/principal.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/jquery.bsPhotoGallery.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+	<link  rel = " stylesheet " href = " https://unpkg.com/aos@next/dist/aos.css " />
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/principal.js"></script>
+    <script src="js/jquery.bsPhotoGallery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://smtpjs.com/v3/smtp.js"></script>
     <TItle>Procem Internacional</TItle>
 
-<!-- FINAL DEL HEAD INICIO DEL BODY HEADER  -->
-    <?php include 'views/Partials/header.php'; ?>
-<!-- TAMBIEN INCLUYE EL FINAL DEL  HEADER  -->
-<div class="carrouselpad"><!-- INICIO DEL INVENTO -->
+</HEAD>
+<body>
 
+<img src="img/reconstruccion.png" class="w-100">
 
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="img/culturaempresarial/expo5.png" class="plearning_banner d-block w-100" style="padding-left: 20%;padding-right: 20%;">
-        </div>
-        <div class="carousel-item">
-        <img src="img/culturaempresarial/expo6.png" class="plearning_banner d-block w-100" style="padding-left: 20%;padding-right: 20%;">
-        </div>
-        <div class="carousel-item">
-        <img src="img/culturaempresarial/baner2.png" class="plearning_banner d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="img/culturaempresarial/baner3.png" class="plearning_banner d-block w-100" alt="...">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-    </a>
-    </div>
-
-    <!-- PRIMERA PARTE -->
-    <DIV id="principal" class="par2 hvr-underline-from-center">
-        <div class="container">
-            <div class="general">
-                <h3>SOMOS PROCEM</h3>
-              <div data-aos="fade-up" data-aos-duration="2000">
-                <h5 class=" parrafo1">La Fundación Procem Internacional de Panamá es una organización sin fines de lucro, que como sus siglas lo indican somos una fundación Pro Cultura Empresarial</h5>
-              </div>
-              </div>
-        <div class="general">
-        <div class="row">
-                <div class="col-sm-6">
-                    <div class="imagen_principal">
-                    <img class="principal" src="img/culturaempresarial/procem.png" alt="">
-                    </div>
-                </div>
-          
-                <div class="col-lg-6" >
-                  <div data-aos="fade-up" data-aos-duration="3000">
-                    <h3>LEY 128 DEL 3 DE MARZO DE 2020</h3>
-                    <p class="parrafo1">Luego de 15 años de gestionar el proyecto ley, se hace realidad, como una herramienta educativa cónsona con la realidad global.
-                        Esta ley establece la enseñanza obligatoria de la Cultura Empresarial y del Emprendimiento como eje transversal en asignaturas.
-                    </p>
-                    <div style="text-align: center">
-                        <a id="imagen" href="views/principal/leer_mas.php?nombre=lee_procem" class="boton btn btn-primary">Leer más...</a>
-                    </div>
-                </div>
-          </div>
-                </div>
-            </div>
-
-        </div>
-    </DIV>
-    <!-- FIN PRIMERA PARTE -->
-    <!-- SEGUNDA PARTE -->
-    <DIV id="second" class="none2 hvr-underline-from-center2">
-            <div class="centro panelsombreado">
-            <div class="container">
-
-                <h3>PROCEM EN PRO DE LA EDUCACIÓN</h3>
-                <div class="general">
-                  <div data-aos="fade-up" data-aos-duration="3000">
-                <p class=" parrafo2">La Fundación Procem funciona para la educación internacional buscando enseñar, capacitar, asesorar y dirigir la Cultura Empresarial a través de la metodología Triangular de las Artes, inspirada por la Dra. AnaMae de Barbosa de Brasil</p>
-                  </div>
-                </div>
-                <div class="filas "></div>
-            </div>
-            </div>
-    </DIV>
-    <!-- FIN SEGUNDA PARTE -->
-    <!-- CUARTA  PARTE -->
-    <DIV id="third" class="par">
-        <div class="container">
-        <div class="general">
-            <div class="row">
-               
-                <div class="col-md-12 mt-md-0 mt-4">
-                	<h3 class="centro"><img class="principal" src="img/procem/pcampus.png" style="width: 40%;height: 15%;" alt=""></h3>
-                    <div class="general">
-                      <div data-aos="fade-up" data-aos-duration="3000">
-                          <p class=" parrafo3">La plataforma educacional "P-CAMPUS" será la mejor escuela internacional para nuestros niños, jóvenes y adultos con una educación integral,<br> con el fin de tener a Panamá por encima de todos los países en la educación virtual y semi presencial.
-                          </p>
-                      </div>
-                    </div>
-                  
-                </div>
-                
-            </div>
-            </div> 
-        </div>
-    </DIV>
-    <!-- FIN CUARTA PARTE -->
-    <!-- TERCERA PARTE -->
-    <DIV  id= "fourth" class="none hvr-underline-from-center">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 mt-md-0 mt-6">
-                    <div class="centro">
-                        <div class="general">
-                        <h3 style="color: white;">¿QUÉ ES LA CULTURA EMPRESARIAL?</h3>
-                          <div data-aos="fade-up" data-aos-duration="1000">
-                        <p class=" parrafo4">La cultura Empresarial es el ADN de todo ser humano individual en su mundo cotidiano, comercial, técnico y científico mediante la creatividad, organizacional y productividad.</p>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 mt-md-0 mt-6">
-                    <div class="centro">
-                        <div class="general">
-                            <div class="columnas">
-                            <h3 style="color: white;">¿CÓMO IMPACTA EN LA JUVENTUD?</h3>
-                              <div data-aos="fade-up" data-aos-duration="3000">
-                            <p class=" parrafo4">La Cultura Empresarial impacta a la juventud en toda su organización como ser pensante, analítico y crítico.
-                                Por medio del estudio profundo de su ser o como hoy día se refiere a su marca. La juventud está preparada para
-                                enfrentarse a situaciones adversas con una Cultura Empresarial.
-                            </p>
-                              </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </DIV>
-    <!-- FIN TERCERA PARTE -->
-
-
-    <!-- QUINTA PARTE -->
-    <DIV id="fifth" class="par ">
-        <div class="container">
-            <div class="row">
-                
-                <div class="col-12">
-                    <div class="centro">
-                        <div class="general">
-                        <h3>¿CÓMO PUEDO SER PARTE DE PROCEM?</h3>
-                          <div data-aos="fade-up" data-aos-duration="3000">
-                        <p class=" parrafo5">Todos pueden ser parte de PROCEM, como estudiantes, maestros y profesionales en diferentes ramas como padrinos, inversionistas y voluntarios. La Fundación Procem Internacional necesita del interés internacional de productividad en contra de la pobreza.</p>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12" style="text-align:center;">
-                    <img class="principal2" src="img/f-icons/5.png" alt="">
-                </div>
-
-            </div>
-        </div>
-    </DIV>
-</div>
-<!-- FIN DEL INVENTO -->
-
-<!-- FIN QUINTA PARTE -->
-<?php include 'views/Partials/footer.php'; ?>
 </body>
 </HTML>
